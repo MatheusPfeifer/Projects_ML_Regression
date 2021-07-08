3b46ef1d3d5d7605805e48a73718856c3bf83765
Matheus Pfeifer <66805980+MatheusPfeifer@users.noreply.github.com>
2021-07-08 09:31:26 -0300

    Update app.py

--- Predict-Price/app.py
+++ Predict-Price/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from pycaret.regression import *
 
 # loading the trained model.
-model = load_model('model/my-modelo-final.pkl')
+model = load_model('model/my-modelo-final')
 
 # carregando uma amostra dos dados.
 dataset = pd.read_csv('data/dataset.csv') 
